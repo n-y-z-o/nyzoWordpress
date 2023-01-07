9b28128a80b2014f8524edb456ce760b083c1907
Nyzo <nyzo@nyzo.co>
2023-01-06 20:38:31 -0500

    Added initial plugin file.

---
+++ nyzo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Nyzo
+Author: Nyzo
+Author URI: http://tech.nyzo.co
+Description: This plugin is being developed to enable easier integration of Nyzo Micropay and related functionalities on WordPress sites.
+Version: 0.0.1
+License: The Unlicense
+ */
+
