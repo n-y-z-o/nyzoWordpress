eff3ab36c1cfd3d1d95e9af143514b93bfba607a
Nyzo <nyzo@nyzo.co>
2023-01-15 10:47:01 -0500

    Added passFail() function to NyzoTestUtil class.

--- tests/nyzoTestUtil.php
+++ tests/nyzoTestUtil.php
@@ -8,6 +8,13 @@ require_once(__TEST_DIRECTORY__ . '/nyzoStringTest.php');
 
 class NyzoTestUtil {
 
+    // TODO: replicate the ConsoleColor class from the Java codebase
+    const SUCCESS_DARK = "\e[42m\e[97m";  // dark green, bright white letters
+    const SUCCESS_LIGHT = "\e[102m\e[30m";  // bright green, black letters
+    const FAILURE_DARK = "\e[41m\e[97m";  // dark red, bright white letters
+    const FAILURE_LIGHT = "\e[101m\e[30m";  // bright red, black letters
+    const CONSOLE_RESET = "\e[0m";
+
     static function run() {
         echo '*****************' . PHP_EOL;
         echo '* running tests *' . PHP_EOL;
@@ -16,6 +23,22 @@ class NyzoTestUtil {
         NyzoStringTest::run();
 
         echo PHP_EOL;
+    }
+
+    static function passFail(bool $successful): string {
+
+        // Get information about the calling function.
+        $backtrace = debug_backtrace(!DEBUG_BACKTRACE_PROVIDE_OBJECT|DEBUG_BACKTRACE_IGNORE_ARGS,2)[1];
+
+        // Build and return the result.
+        if ($successful) {
+            $result = NyzoTestUtil::SUCCESS_DARK . '++PASS++' . NyzoTestUtil::SUCCESS_LIGHT . ' ' .
+                $backtrace['class'] . $backtrace['type'] . $backtrace['function'] . '()' . NyzoTestUtil::CONSOLE_RESET;
+        } else {
+            $result = NyzoTestUtil::FAILURE_DARK . '--FAIL--' . NyzoTestUtil::FAILURE_LIGHT . ' ' .
+                $backtrace['class'] . $backtrace['type'] . $backtrace['function'] . '()' . NyzoTestUtil::CONSOLE_RESET;
+        }
+        return $result;
     }
 }
 
