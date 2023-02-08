ba1440a86498e41d5b3c7f6f943dc988c59d0aa1
Nyzo <nyzo@nyzo.co>
2023-02-08 07:46:11 -0500

    Added file for JavaScript tests.

---
+++ javascript/nyzoTestUtil.js
@@ -0,0 +1,5 @@
+function runNyzoTests(targetDiv) {
+    targetDiv.innerHTML = 'This div will display the results of the tests.';
+    targetDiv.style.color = 'gray';
+    targetDiv.style.fontStyle = 'italic';
+}
