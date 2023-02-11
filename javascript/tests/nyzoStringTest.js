36f47c27ff861fd524f2c04639bc0c73689bd304
Nyzo <nyzo@nyzo.co>
2023-02-11 17:10:57 -0500

    Added structure for JavaScript test result table.

--- javascript/tests/nyzoStringTest.js
+++ javascript/tests/nyzoStringTest.js
@@ -4,6 +4,12 @@ class NyzoStringTest {
     }
 
     run() {
-        return true;
+        let result = [];
+
+        result.push({ name: 'Test 1', result: 'test 1 was successful', successful: true });
+        result.push({ name: 'Test 2', result: 'test 2 was successful', successful: true });
+        result.push({ name: 'Test 3', result: 'test 3 was not successful', successful: false });
+
+        return result;
     }
 }
--- javascript/tests/nyzoTestUtil.js
+++ javascript/tests/nyzoTestUtil.js
@@ -26,19 +26,25 @@ function runNyzoTests(targetDiv) {
     // Run the tests.
     let successful = true;
     for (let test of tests) {
-        // Add the result row to the table.
-        const resultRow = document.createElement('div');
-        resultRow.className = 'data-row';
-        table.appendChild(resultRow);
+        const results = test.run();
+        for (let result of results) {
+            console.log('result: ' + result);
 
-        // Add the test name to the result row.
-        const nameDiv = document.createElement('div');
-        nameDiv.textContent = test.constructor.name;
-        resultRow.appendChild(nameDiv);
+            // Add the result row to the table.
+            const resultRow = document.createElement('div');
+            resultRow.className = 'data-row';
+            table.appendChild(resultRow);
 
-        // Add the test result to the result row.
-        const resultDiv = document.createElement('div');
-        resultDiv.textContent = '-';
-        resultRow.appendChild(resultDiv);
+            // Add the test name to the result row.
+            const nameDiv = document.createElement('div');
+            nameDiv.textContent = result.name;
+            resultRow.appendChild(nameDiv);
+
+            // Add the test result to the result row.
+            const resultDiv = document.createElement('div');
+            resultDiv.className = result.successful ? 'test-successful' : 'test-unsuccessful';
+            resultDiv.textContent = result.result;
+            resultRow.appendChild(resultDiv);
+        }
     }
 }
