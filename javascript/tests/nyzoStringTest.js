efbe5ce951e0c33041554012caf628a065319f29
Nyzo <nyzo@nyzo.co>
2023-02-12 22:01:40 -0500

    Added NyzoStringTest.testEncoder().

--- javascript/tests/nyzoStringTest.js
+++ javascript/tests/nyzoStringTest.js
@@ -6,10 +6,49 @@ class NyzoStringTest {
     run() {
         let result = [];
 
-        result.push({ name: 'Test 1', result: 'test 1 was successful', successful: true });
-        result.push({ name: 'Test 2', result: 'test 2 was successful', successful: true });
-        result.push({ name: 'Test 3', result: 'test 3 was not successful', successful: false });
+        result.push(this.testEncoder());
 
         return result;
+    }
+
+    testEncoder() {
+        // The encoding lookup table is 64 characters. 64 characters store 6 bits (2^6=64). This allows the encoder to
+        // store 3 bytes (24 bits) in 4 characters. This test checks encoding and decoding up to double this packing
+        // increment, from 1 to 6 bytes (1 to 8 characters).
+
+        const byteArrays = [ '89', '712aab', '3a0536', 'bd53aa', '2e', '98', '9d65ed', '1bdd1542ca', '0b49ddab',
+            '78fdccc42427', '994d0200', '0e0000', '40', 'e6093ad212', '19941b43', 'e5221d048a', 'b419',  '65bb880d55',
+            '6a2b74e7f9', '595c', 'db851f52', '7193', 'a817', '82', '9f2e4dee75f2', 'de805a40e3e9', '2920d328b493',
+            '5151', '1e1345be', '826b82f468dd' ];
+        const strings = [ 'zg', 'tiHI', 'exkU', 'MmeH', 'bx', 'D0', 'EnoK', '6.SmgJF', '2SEuHN', 'vfVcP2gE', 'DkS200',
+            '3x00', 'g0', 'XxBYSy8', '6qgsgN', 'Xi8u18F', 'K1B', 'qsL83mk', 'rzKSX_B', 'nmN', 'UWkwkx', 'tqc', 'H1t',
+            'xx', 'EQXdZEoQ', 'VF1rgefG', 'ai3jabij', 'km4', '7yd5Mx', 'xDL2.6Au' ];
+
+        // Check decoding and encoding for all values.
+        let successful = true;
+        let failureCause = 'successful';
+        for (let i = 0; i < byteArrays.length && successful; i++) {
+            const byteArray = hexStringAsUint8Array(byteArrays[i]);
+            const string = strings[i];
+
+            // Check decoding against the expected byte array.
+            const decodedByteArray = byteArrayForEncodedString(string);
+            if (!arraysAreEqual(byteArray, decodedByteArray)) {
+                successful = false;
+                failureCause = 'mismatch of expected byte array (' + byteArray +
+                    ') and decoded byte array (' + decodedByteArray + ') in iteration ' + i +
+                    ' of NyzoStringTest.testEncoder()';
+            }
+
+            // Check encoding against the expected encoded string.
+            const encodedString = encodedStringForByteArray(byteArray);
+            if (string != encodedString) {
+                successful = false;
+                failureCause = 'mismatch of expected string (' + string + ') and encoded string (' + encodedString +
+                    ') in iteration ' + i + ' of NyzoStringTest.testEncoder()';
+            }
+        }
+
+        return { name: 'NyzoStringTest.testEncoder()', result: failureCause, successful: successful }
     }
 }
