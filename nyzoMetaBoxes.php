77398da55e8b8db5b8a056d215bf7c632630823a
Nyzo <nyzo@nyzo.co>
2023-02-27 19:07:25 -0500

    Added post option for number of preview blocks.

--- nyzoMetaBoxes.php
+++ nyzoMetaBoxes.php
@@ -18,6 +18,17 @@ function nyzo_meta_boxes_elements() {
 
     wp_nonce_field(plugin_basename(__FILE__), 'nyzo_nonce');
 
+    echo '<div class="components-panel__row">';
+    echo '<div class="components-base-control components-checkbox-control">';
     echo '<input id="nyzo_monetize_with_micropay" name="nyzo_monetize_with_micropay" type="checkbox" value="1" />';
     echo '<label for="nyzo_monetize_with_micropay">Monetize with Micropay</label>';
+    echo '</div>';
+    echo '</div>';
+
+    echo '<div class="components-panel__row">';
+    echo '<div class="components-base-control components-checkbox-control">';
+    echo '<label for="nyzo_preview_block_count">Number of preview blocks</label>';
+    echo '<input id="nyzo_preview_block_count" name="nyzo_preview_block_count" type="number" min="0" />';
+    echo '</div>';
+    echo '</div>';
 }
