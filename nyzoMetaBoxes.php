1cf5e5e8284a0f157d5e2615cdcdb35e3163841d
Nyzo <nyzo@nyzo.co>
2023-03-01 22:33:02 -0500

    Saving and retrieving preview block count.

--- nyzoMetaBoxes.php
+++ nyzoMetaBoxes.php
@@ -29,10 +29,16 @@ function nyzo_meta_boxes_elements($post) {
     echo '</div>';
     echo '</div>';
 
+    $previewBlockCount = get_post_meta($post->ID, 'nyzo_preview_block_count', true);
+    if (!is_numeric($previewBlockCount) || $previewBlockCount < 0) {
+        $previewBlockCount = 3;
+    }
+
     echo '<div class="components-panel__row">';
     echo '<div class="components-base-control components-checkbox-control">';
     echo '<label for="nyzo_preview_block_count">Number of preview blocks</label>';
-    echo '<input id="nyzo_preview_block_count" name="nyzo_preview_block_count" type="number" min="0" />';
+    echo '<input id="nyzo_preview_block_count" name="nyzo_preview_block_count" type="number" min="0" value="' .
+        $previewBlockCount . '"/>';
     echo '</div>';
     echo '</div>';
 }
@@ -42,10 +48,17 @@ function nyzo_save_metadata($post_id) {
     if (isset($_POST['nyzo_nonce']) && wp_verify_nonce($_POST['nyzo_nonce'], 'nyzo_post_meta') &&
         current_user_can('edit_post', $post_id)) {
 
+        // Save the monetization option.
         if (isset($_POST['nyzo_monetize_with_micropay'])) {
             update_post_meta($post_id, 'nyzo_monetize_with_micropay', '1');
         } else {
             delete_post_meta($post_id, 'nyzo_monetize_with_micropay');
+        }
+
+        // Save the number of preview blocks.
+        if (isset($_POST['nyzo_preview_block_count']) && is_numeric($_POST['nyzo_preview_block_count']) &&
+            $_POST['nyzo_preview_block_count'] >= 0) {
+            update_post_meta($post_id, 'nyzo_preview_block_count', $_POST['nyzo_preview_block_count']);
         }
     }
 }
